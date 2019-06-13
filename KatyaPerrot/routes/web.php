b0ff53bb0313ff240ff72b367fc1cd808128b9ff
nvmoiseikin <nvmoiseikin@gmail.com>
2019-06-13 15:01:47 +0500

    route capthca activate

--- KatyaPerrot/routes/web.php
+++ KatyaPerrot/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 
 Route::post('/contactUs', 'ControllerContactUs@ship')->name('contactUs');
 
+
+Route::get('/get_captcha/{config?}', function (\Mews\Captcha\Captcha $captcha, $config = 'flat') {
+    return $captcha->src($config);
+});
 /*** BUY
 Route::get('/before_buy', function () {return view('before_buy');})->name('before_buy');
 
@@ -30,9 +34,7 @@ Route::post('/receive', 'ControllerBuy@yandexReceive')->name('receive');
 
 Route::get('/buy', 'ControllerBuy@buy')->name('buy');
 
-Route::get('/get_captcha/{config?}', function (\Mews\Captcha\Captcha $captcha, $config = 'flat') {
-    return $captcha->src($config);
-});
+
     $router->post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
     $router->post('password/reset', 'ResetPasswordController@reset');
     
